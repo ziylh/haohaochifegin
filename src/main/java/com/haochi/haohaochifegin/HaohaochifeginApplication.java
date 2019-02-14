063398a596542c2b4f00ba6a26cda414f084424f
用户名 <1262967980@qq.com>
2019-02-14 20:15:15 +0800

    fegin

---
+++ src/main/java/com/haochi/haohaochifegin/HaohaochifeginApplication.java
@@ -0,0 +1,14 @@
+package com.haochi.haohaochifegin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaohaochifeginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaohaochifeginApplication.class, args);
+    }
+
+}
+
